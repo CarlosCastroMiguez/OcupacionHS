3e8c0259efa781a080ddea8b1cd60b2e23f9ec21
CarlosCastroMiguez <karloskastro12@gmail.com>
2019-07-01 18:16:47 +0200

    Solucionamos problemas footer
    .

--- resources/views/admin/eventos/importar.blade.php
+++ resources/views/admin/eventos/importar.blade.php
@@ -4,6 +4,8 @@
 
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
 
+<link rel="stylesheet" href="/css/importar.css" />
+
 @if(session('notification'))
 <div class="alert alert-success">
     {{ session('notification') }}
